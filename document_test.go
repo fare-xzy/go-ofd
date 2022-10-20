2f484348303c4b7576d7fd89f6a80933cb8cf5fd
xuziyu <xuziyu@bjca.org.cn>
2022-10-20 19:19:36 +0800

    document

---
+++ document_test.go
@@ -0,0 +1,5 @@
+package main
+
+const DocumentContent = `<?xml version="1.0" encoding="UTF-8"?>
+<ofd:Document xmlns:ofd="http://www.ofdspec.org/2016"><ofd:CommonData><ofd:MaxUnitID>368</ofd:MaxUnitID><ofd:PageArea><ofd:PhysicalBox>0 0 210 297</ofd:PhysicalBox></ofd:PageArea><ofd:PublicRes>PublicRes.xml</ofd:PublicRes></ofd:CommonData><ofd:Pages><ofd:Page ID="1" BaseLoc="Pages/Page_0/Content.xml"/><ofd:Page ID="2" BaseLoc="Pages/Page_1/Content.xml"/></ofd:Pages><ofd:CustomTags>Tags/CustomTags.xml</ofd:CustomTags><ofd:VPreferences><TabDisplay>FileName</TabDisplay><HideToolbar>false</HideToolbar><HideMenubar>false</HideMenubar><HideWindowUI>false</HideWindowUI><ZoomMode>0.000000</ZoomMode><Zoom>0.000000</Zoom></ofd:VPreferences></ofd:Document>
+`
